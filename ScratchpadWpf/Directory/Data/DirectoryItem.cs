1d2aa6e8f1629f7d890dbc6f9ff83fbca4dd9a24
Sean Smith <ssmith73@gmail.com>
2018-05-09 09:35:06 +0100

    Just starting work on view model of the directory tree program

---
+++ ScratchpadWpf/Directory/Data/DirectoryItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScratchpadWpf.Directory.Data
+{
+    public class DirectoryItem
+    {
+        /// <summary>
+        ///  The type of this item
+        /// </summary>
+        public DirectoryItemType Type { get; set; }
+
+        /// <summary>
+        /// The absolute path to this item
+        /// </summary>
+        public string FullPath { get; set; }
+
+        /// <summary>
+        /// The name of the directory item
+        /// </summary>
+        public string Name { get { return DirectoryStructure.GetFileFolderName(this.FullPath); } }
+    }
+}
---
+++ ScratchpadWpf/Directory/DirectoryStructure.cs
@@ -0,0 +1,60 @@
+﻿using ScratchpadWpf.Directory.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScratchpadWpf.Directory
+{
+    public static class DirectoryStructure
+    {
+        public static List<DirectoryItem> GetLogicalDrives();
+        #region Window Loaded
+            //Get every logic drive on the machine
+        foreach (var drive in system.io.Directory.GetLogicalDrives())
+        {
+            //Create a new item for it
+            var item = new TreeViewItem
+            {
+                //Set the header and path
+                Header = drive,
+                Tag = drive
+            };
+            //add a dummy item (shows expand as a null item - no text on icon)
+            item.Items.Add(null);
+
+            //listen out for item being expanded
+            item.Expanded += Folder_Expanded;
+            // Add drive to the main tree view
+            FolderView.Items.Add(item);
+        }
+        #endregion
+
+        #region Helpers
+
+        //Find file or folder name from a full path
+        public static string GetFileFolderName(string directoryPath)
+        {
+            //c:\Something\a folder
+            //c:\Something\a file.png
+            // a file file.png (no \    test for this)
+            if (string.IsNullOrEmpty(directoryPath))
+                return string.Empty;
+
+            //make all slashes back slashes
+            var normalisedPath = directoryPath.Replace('/', '\\');
+
+            // (find last \ in pathname)
+            var lastIndex = normalisedPath.LastIndexOf('\\');
+
+            // if we don't find a \ return just the path
+            if (lastIndex <= 0)
+                return directoryPath;
+
+            //return the name after the last backslash
+            return directoryPath.Substring(lastIndex + 1);
+        }       
+        #endregion
+    }
+}
